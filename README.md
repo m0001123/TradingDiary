4e9408bd8dfe68292177db1a656ff2bafce68e25
m0001123 <m220131133@gmail.com>
2024-07-03 01:02:09 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# trading_diary_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/trading_diary_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.trading_diary_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
